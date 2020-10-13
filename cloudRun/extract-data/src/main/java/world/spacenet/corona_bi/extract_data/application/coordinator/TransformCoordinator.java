5e9a8f00ba2c068ce358d2f561f91af4b5a9e573
SpaceNet <spacenet2032@gmail.com>
2020-10-13 14:37:18 +0900

    Polish

--- cloudRun/extract-data/src/main/java/world/spacenet/corona_bi/extract_data/application/coordinator/TransformCoordinator.java
+++ cloudRun/extract-data/src/main/java/world/spacenet/corona_bi/extract_data/application/coordinator/TransformCoordinator.java
@@ -2,9 +2,6 @@ package world.spacenet.corona_bi.extract_data.application.coordinator;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
